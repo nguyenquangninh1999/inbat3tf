545ac96ffeff858b3d2bac41b9de2b20b65fa364
Nguyễn Quang Ninh <ninh.nguyenquang@educo.edu.vn>
2026-04-18 14:45:01 +0700

    main

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
     protected $fillable = ['name', 'price', 'description', 'link_video', 'image'];
+
+    protected static function booted(): void
+    {
+        static::deleting(function (Product $product) {
+            if ($product->image) {
+                Storage::disk('public')->delete($product->image);
+            }
+        });
+    }
 }
